da68aac096d1ca84c6a51fee94a87ddc88edd13b
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-10-22 16:18:07 +0400

    nix: exposed testVM on flake

--- flake.nix
+++ flake.nix
@@ -17,19 +17,38 @@
     nixpkgs,
     ...
   } @ inputs: let
-    inherit (nixpkgs.lib) getAttrs mapAttrs;
+    inherit (nixpkgs.lib) getAttrs mapAttrs nixosSystem;
     pkgsFor = getAttrs ["x86_64-linux" "aarch64-linux"] nixpkgs.legacyPackages;
     eachSystem = fn: mapAttrs fn pkgsFor;
+    specialArgs = {inherit inputs self;};
   in {
     checks = eachSystem (_: pkgs: {
-      hjem-test = pkgs.callPackage ./nix/test {
-        specialArgs = {inherit inputs self;};
-      };
+      hjem-test = pkgs.callPackage ./nix/test {inherit specialArgs;};
     });
 
     hjemModules = {
       hjem-impure = ./nix/module.nix;
       default = self.hjemModules.hjem-impure;
+    };
+
+    # $ nixos-rebuild --flake .#testVm build-vm
+    # $ ./result/bin/run-nixos-vm
+    nixosConfigurations.testVm = nixosSystem {
+      system = "x86_64-linux";
+      inherit specialArgs;
+      modules = [
+        ./nix/test/config.nix
+        ({modulesPath, ...}: {
+          imports = [
+            (modulesPath + "/profiles/qemu-guest.nix")
+            (modulesPath + "/virtualisation/qemu-vm.nix")
+          ];
+
+          virtualisation.graphics = false;
+          users.users.kokomi.initialPassword = "kokomi";
+          hjem.users.kokomi.clobberFiles = true;
+        })
+      ];
     };
   };
 }
